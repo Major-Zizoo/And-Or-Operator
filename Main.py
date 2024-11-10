25196d9603ec21da74b7626d03d56eca5080d269
Major-Zizoo <norelgndy@gmail.com>
2024-11-10 16:25:36 +0300

    7

---
+++ Main.py
@@ -0,0 +1,17 @@
+A=10
+B=15
+C=0
+
+if A and B and C:
+    print("All your numbers are true")
+else:
+    print("One of the numbers is false")
+
+A=10
+B=-10
+C=0
+
+if A >0 or B>0:
+    print("A or B is greater than 0")
+else:
+    print("No number is greater than 0.")
